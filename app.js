76f489fa29e87a43e4d071411288a00d667514fa
Sagi <sagi403@gmail.com>
2023-06-26 14:22:05 +0300

    Adding cors

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(helmet());
+var corsOptions = {
+  origin: ["http://localhost:3000"],
+};
 
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(express.json());
 
 // Routes
